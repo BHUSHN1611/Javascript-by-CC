132f805472fb84be905777f88efd191dd485dd69
BHUSHN1611 <bhushanjadhav1603@gmail.com>
2024-08-20 10:59:26 +0000

    comparison

---
+++ 01_basics/compare.js
@@ -0,0 +1,34 @@
+// comparison operator
+
+console.log(2>1) // comparison operator
+console.log(2<5)
+console.log(1==1) // equality check 
+console.log(1!=0)
+
+// comparing Null to number 
+
+console.log(null > 0) // convert null in to 0 
+console.log(null < 0) 
+
+console.log(null == 0) // anykind of conversion  not done here
+
+let a = null 
+let nullValue = Number(a)
+
+console.log(nullValue == 0)
+
+console.log(undefined>0)
+console.log(undefined<0)
+console.log(undefined==0)
+
+
+let e =undefined
+let n = Number(e)
+console.log(n)
+console.log(n==0)
+
+// strick equality check
+
+console.log("Strict Equality check ","2" === 2)
+
+// dont folow this type of comparison
